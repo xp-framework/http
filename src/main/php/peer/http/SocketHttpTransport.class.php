9b5680f312d182b326d7f7354c5a431d9c044818
Timm Friebe <thekid@thekid.de>
2014-10-14 20:05:00 +0200

    Implement using "CONNECT" for tunneling HTTPS through proxy
    Does not work, yields a "Bad request" on the system I am trying it out
    with at the moment... though looking at other implementations it seems
    to be correct. Must be missing something, though not sure what exactly

--- src/main/php/peer/http/SocketHttpTransport.class.php
+++ src/main/php/peer/http/SocketHttpTransport.class.php
@@ -44,6 +44,41 @@ class SocketHttpTransport extends HttpTransport {
     parent::setProxy($proxy);
     $this->proxySocket= new Socket($proxy->host, $proxy->port);
   }
+
+  /**
+   * Connect to a socket. If socket still open from last request (which
+   * is the case when unread data is left on it by not reading the body,
+   * e.g.), use the quick & dirty way: Close and reopen!
+   *
+   * @param  peer.Socket $s
+   * @param  double $read Read timeout
+   * @param  double $connect Connect timeout
+   * @return peer.Socket
+   */
+  protected function connect($s, $read, $connect) {
+    $s->isConnected() && $s->close();
+    $s->setTimeout($read);
+    $s->connect($connect);
+    return $s;
+  }
+
+  /**
+   * Enable cryptography on a given socket
+   *
+   * @param  peer.Socket $s
+   * @param  [:int] $methods
+   * @return void
+   * @throws io.IOException
+   */
+  protected function enable($s, $methods) {
+    foreach ($methods as $name => $method) {
+      if (stream_socket_enable_crypto($s->getHandle(), true, $method)) {
+        $this->cat && $this->cat->info('@@@ Enabling', $name, 'cryptography');
+        return;
+      }
+    }
+    throw new \io\IOException('Cannot establish secure connection, tried '.\xp::stringOf($methods));
+  }
   
   /**
    * Sends a request
@@ -56,28 +91,44 @@ class SocketHttpTransport extends HttpTransport {
   public function send(HttpRequest $request, $timeout= 60, $connecttimeout= 2.0) {
 
     // Use proxy socket and Modify target if a proxy is to be used for this request, 
-    // a proxy wants "GET http://example.com/ HTTP/X.X"
+    // a proxy wants "GET http://example.com/ HTTP/X.X" for (and "CONNECT" for HTTPs).
     if ($this->proxy && !$this->proxy->isExcluded($url= $request->getUrl())) {
-      $request->setTarget(sprintf(
-        '%s://%s%s%s',
-        $url->getScheme(),
-        $url->getHost(),
-        $url->getPort() ? ':'.$url->getPort() : '',
-        $url->getPath('/')
-      ));
-
-      $s= $this->proxySocket;
+      $s= $this->connect($this->proxySocket, $timeout, $connecttimeout);
+      if ('http' === $url->getScheme()) {
+        $request->setTarget(sprintf(
+          '%s://%s%s%s',
+          $url->getScheme(),
+          $url->getHost(),
+          $url->getPort() ? ':'.$url->getPort() : '',
+          $url->getPath('/')
+        ));
+      } else {
+        $connect= sprintf(
+          "CONNECT %1\$s:%2\$d HTTP/1.1\r\nHost: %1\$s:%2\$d\r\n\r\n",
+          $url->getHost(),
+          $url->getPort(443)
+        );
+        $this->cat && $this->cat->info('>>>', $connect);
+        $s->write($connect);
+        $handshake= $s->read();
+        $this->cat && $this->cat->info('<<<', $handshake);
+        sscanf($handshake, "HTTP/%*d.%*d %d %[^\r]", $status, $message);
+        if (200 === $status) {
+          $s->read();
+          $this->enable($s, [
+            'TLS'    => STREAM_CRYPTO_METHOD_TLS_CLIENT,
+            'SSLv3'  => STREAM_CRYPTO_METHOD_SSLv3_CLIENT,
+            'SSLv23' > STREAM_CRYPTO_METHOD_SSLv23_CLIENT,
+            'SSLv2'  => STREAM_CRYPTO_METHOD_SSLv2_CLIENT
+          ]);
+        } else {
+          return new HttpResponse(new \io\streams\MemoryInputStream($handshake));
+        }
+      }
     } else {
-      $s= $this->socket;
+      $s= $this->connect($this->socket, $timeout, $connecttimeout);
     }
-    
-    // Socket still open from last request. This is the case when unread
-    // data is left on the socket (by not reading the body, e.g.), so do
-    // it the quick & dirty way: Close and reopen!
-    $s->isConnected() && $s->close();
-  
-    $s->setTimeout($timeout);
-    $s->connect($connecttimeout);
+
     $s->write($request->getRequestString());
 
     $this->cat && $this->cat->info('>>>', $request->getHeaderString());
