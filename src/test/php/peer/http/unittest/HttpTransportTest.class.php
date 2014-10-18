93be709704dbbb073b7209bab015ac57aeadb874
Timm Friebe <thekid@thekid.de>
2014-10-18 17:04:55 +0200

    Add tests for proxy usage

--- src/test/php/peer/http/unittest/HttpTransportTest.class.php
+++ src/test/php/peer/http/unittest/HttpTransportTest.class.php
@@ -1,14 +1,15 @@
 <?php namespace peer\http\unittest;
 
-use unittest\TestCase;
+use peer\URL;
 use peer\http\HttpTransport;
+use peer\http\HttpProxy;
 
 /**
  * TestCase
  *
  * @see      xp://peer.http.HttpTransport
  */
-class HttpTransportTest extends TestCase {
+class HttpTransportTest extends \unittest\TestCase {
 
   /**
    * Register test transport
@@ -16,12 +17,16 @@ class HttpTransportTest extends TestCase {
   #[@beforeClass]
   public static function registerTransport() {
     HttpTransport::register('test', \lang\ClassLoader::defineClass('TestHttpTransport', 'peer.http.HttpTransport', array(), '{
-      public $host, $port, $arg;
+      public $host, $port, $arg, $proxy;
 
       public function __construct(\peer\URL $url, $arg) {
         $this->host= $url->getHost();
         $this->port= $url->getPort(80);
         $this->arg= $arg;
+      }
+
+      public function setProxy(\peer\http\HttpProxy $proxy= null) {
+        $this->proxy= $proxy;
       }
       
       public function send(\peer\http\HttpRequest $request, $timeout= 60, $connecttimeout= 2.0) {
@@ -36,26 +41,62 @@ class HttpTransportTest extends TestCase {
   }
 
   #[@test]
-  public function port80IsDefaultPort() {
-    $t= HttpTransport::transportFor(new \peer\URL('test://example.com'));
+  public function host() {
+    $t= HttpTransport::transportFor(new URL('test://example.com'));
     $this->assertEquals('example.com', $t->host);
+  }
+
+  #[@test]
+  public function port_80_is_the_default_port() {
+    $t= HttpTransport::transportFor(new URL('test://example.com'));
     $this->assertEquals(80, $t->port);
-    $this->assertEquals(null, $t->arg);
   }
 
   #[@test]
-  public function withPort() {
-    $t= HttpTransport::transportFor(new \peer\URL('test://example.com:8080'));
-    $this->assertEquals('example.com', $t->host);
+  public function urls_may_contain_port() {
+    $t= HttpTransport::transportFor(new URL('test://example.com:8080'));
     $this->assertEquals(8080, $t->port);
-    $this->assertEquals(null, $t->arg);
   }
 
   #[@test]
-  public function withPortAndArg() {
-    $t= HttpTransport::transportFor(new \peer\URL('test+v2://example.com:443'));
-    $this->assertEquals('example.com', $t->host);
-    $this->assertEquals(443, $t->port);
+  public function schemes_may_contain_args() {
+    $t= HttpTransport::transportFor(new URL('test+v2://example.com:443'));
     $this->assertEquals('v2', $t->arg);
+  }
+
+  #[@test]
+  public function null_is_passed_if_scheme_has_no_arg() {
+    $t= HttpTransport::transportFor(new URL('test://example.com:443'));
+    $this->assertNull($t->arg);
+  }
+
+  #[@test]
+  public function scheme_proxy_is_used() {
+    with ($proxy= new HttpProxy('proxy', 3128)); {
+      HttpTransport::$PROXIES['test']= $proxy;
+      $t= HttpTransport::transportFor(new URL('test://example.com'));
+      unset(HttpTransport::$PROXIES['test']);
+      $this->assertEquals($proxy, $t->proxy);
+    }
+  }
+
+  #[@test]
+  public function all_proxy_is_used() {
+    with ($proxy= new HttpProxy('proxy', 3128)); {
+      HttpTransport::$PROXIES['*']= $proxy;
+      $t= HttpTransport::transportFor(new URL('test://example.com'));
+      unset(HttpTransport::$PROXIES['*']);
+      $this->assertEquals($proxy, $t->proxy);
+    }
+  }
+
+  #[@test]
+  public function no_proxy_is_used_if_scheme_does_not_match() {
+    with ($proxy= new HttpProxy('proxy', 3128)); {
+      HttpTransport::$PROXIES['anonther-scheme']= $proxy;
+      $t= HttpTransport::transportFor(new URL('test://example.com'));
+      unset(HttpTransport::$PROXIES['anonther-scheme']);
+      $this->assertNull($t->proxy);
+    }
   }
 }
