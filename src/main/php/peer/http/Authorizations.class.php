104b8aeb5e466c4e538e11545d06d6e7b006b88a
Alex D'Andrea <alex.dandrea@1und1.de>
2015-04-09 08:03:43 +0200

    Refactor Authorizations
    * make non-static
    * make implementations extensible
    * test it

--- src/main/php/peer/http/Authorizations.class.php
+++ src/main/php/peer/http/Authorizations.class.php
@@ -1,6 +1,8 @@
 <?php namespace peer\http;
 
 use lang\Object;
+use lang\XPClass;
+use lang\reflect\TargetInvocationException;
 use security\SecureString;
 use lang\IllegalStateException;
 
@@ -10,6 +12,40 @@ use lang\IllegalStateException;
  */
 class Authorizations extends Object {
   const AUTH_HEADER= 'WWW-Authenticate';
+  protected $impl= [
+    ['startsWith' => 'Basic ', 'impl' => 'peer.http.BasicAuthorization'],
+    ['startsWith' => 'Digest ', 'impl' => 'peer.http.DigestAuthorization']
+  ];
+
+  public function required(HttpResponse $response) {
+    return HttpConstants::STATUS_AUTHORIZATION_REQUIRED == $response->getStatusCode();
+  }
+
+  public function create(HttpResponse $response, $user, SecureString $pass) {
+    if (!$this->required($response)) {
+      throw new IllegalStateException('Request had not been rejected, will not create authorization.');
+    }
+
+    if (1 != sizeof($response->header(self::AUTH_HEADER))) {
+      throw new IllegalStateException('No authentication type indicated.');
+    }
+
+    $header= $response->header(self::AUTH_HEADER)[0];
+    foreach ($this->impl as $impl) {
+      if (0 == strncmp($impl['startsWith'], $header, strlen($impl['startsWith']))) {
+        try {
+          return XPClass::forName($impl['impl'])->getMethod('fromChallenge')->invoke(
+            null,
+            [$header, $user, $pass]
+          );
+        } catch (TargetInvocationException $e) {
+          throw $e->getCause();
+        }
+      }
+    }
+
+    throw new IllegalStateException('Unknown authorization type.');
+  }
 
   /**
    * Create authorization from challenge data from given
@@ -24,27 +60,6 @@ class Authorizations extends Object {
    * @throws lang.IllegalStateException If Unknown authorization type was used
    */
   public static function fromResponse(HttpResponse $response, $user, SecureString $pass) {
-    if (HttpConstants::STATUS_AUTHORIZATION_REQUIRED !== $response->getStatusCode()) {
-      throw new IllegalStateException('Request had not been rejected, will not create authorization.');
-    }
-
-    if (1 != sizeof($response->header(self::AUTH_HEADER))) {
-      throw new IllegalStateException('No authentication type indicated.');
-    }
-
-    $header= $response->header(self::AUTH_HEADER)[0];
-    switch (true) {
-      case 'Digest ' === substr($header, 0, strlen('Digest ')): {
-        return DigestAuthorization::fromChallenge($header, $user, $pass);
-      }
-
-      case 'Basic ' === substr($header, 0, strlen('Basic ')): {
-        return new BasicAuthorization($this->user, $this->pass);
-      }
-
-      default: {
-        throw new IllegalStateException('Unknown authorization type.');
-      }
-    }
+    return (new self())->create($response, $user, $pass);
   }
 }
